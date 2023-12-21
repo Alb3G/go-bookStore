607011665c0cdcd7417621be9e2fcca442cda8dd
Alberto <guzmanmorenoalberto1998@gmail.com>
2023-12-21 10:15:14 +0100

    .env file added to hide the Data base connection path

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,8 +1,12 @@
 package config
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -10,7 +14,20 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:4111@tcp(localhost:3306)/go-bookstore-db?charset=utf8&parseTime=True&loc=Local")
+	envError := godotenv.Load()
+	if envError != nil {
+		panic("Error importing file .env")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dbURI := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	d, err := gorm.Open("mysql", dbURI)
 	if err != nil {
 		panic(err)
 	}
